01d7e1b38410a6f5e74774be18238161e1045093
akash1234556 <113225974+akash1234556@users.noreply.github.com>
2023-12-02 09:07:46 +0530

    Create factorial number.java

---
+++ factorial number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class GFG {
+	static int factorial(int n)
+	{
+
+		if (n == 0 || n == 1)
+			return 1;
+		
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args)
+	{
+
+		int ans = factorial(5);
+
+		System.out.println("Factorial of 5 is :" + ans);
+	}
+}
